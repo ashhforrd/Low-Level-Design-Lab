1ba6a94bf152aee811e99bf41ab04cdfafc5240c
HaydarChan <atqiyahaydar15@gmail.com>
2026-09-26 22:16:22 +0700

    feat: start yoputube learn

---
+++ youtube-learn/elevator.py
@@ -0,0 +1,14 @@
+"""
+Requirements -> Entities -> Class Design -> Implementation -> Extensibility (If time permits)
+
+Design an elevator control system for a building. The system should handle multiple elevator, floor requests, and move elevators efficiently to service requests
+
+Question Type:
+1. primary capabilities
+2. error handling
+3. scope boundaries
+
+
+
+
+"""
